b7664bf9229fc7f24e903d2dd62d6d6ece5c90a2
soggy-cereal1 <114190885+soggy-cereal1@users.noreply.github.com>
2022-12-28 20:45:12 -0500

    Diversejo.py and testingfile.py
    the first should work, the second sends 7 friend reqs to me

---
+++ diversejo.py
@@ -0,0 +1,84 @@
+import pyautogui
+import time
+
+def tabrefresh(index):   
+    pyautogui.hotkey('command', 't') # open new tab when discord stops this from working
+    pyautogui.hotkey('command', '1') # how to implement in below?
+    pyautogui.hotkey('command', 'w')
+
+    time.sleep(1)
+
+    pyautogui.typewrite("https://discord.com/channels/@me \n", 0)
+    time.sleep(10)
+    pyautogui.click(778, 160, 1, 0, button='left') # clicks Add Friend
+    time.sleep(1)
+
+time.sleep(3)
+pyautogui.click(778, 160, 1, 0, button='left') # clicks Add Friend
+# switch to discord.com/channels/@me at that point under All(no DM open)
+
+pyautogui.click(778, 160, 1, 0, button='left') # clicks Add Friend
+for i in range (1,9999):
+    if (len(str(i)) == 1):    
+       if ((i-1)%3 != 0):
+            pyautogui.click(500, 300, 3, 0.2, button='left')
+            pyautogui.hotkey('command', 'a')
+            pyautogui.typewrite(['backspace'], 0)
+            pyautogui.typewrite("DiverseJo#000" + str(i) + "\n", 0)
+            time.sleep(2)
+
+       else:
+            tabrefresh(i)
+
+            pyautogui.click(500, 300, 3, 0.2, button='left')
+            pyautogui.hotkey('command', 'a')
+            pyautogui.typewrite(['backspace'], 0)
+
+            pyautogui.typewrite("DiverseJo#000" + str(i) + "\n", 0)
+            time.sleep(2)
+
+    elif (len(str(i)) == 2):
+        if ((i-1)%3 != 0):
+            pyautogui.click(500, 300, 3, 0.2, button='left')
+            pyautogui.hotkey('command', 'a')
+            pyautogui.typewrite(['backspace'], 0)
+            pyautogui.typewrite("DiverseJo#00" + str(i) + "\n", 0)
+            time.sleep(2)
+        else:
+            tabrefresh(i)
+
+            pyautogui.click(500, 300, 3, 0.2, button='left')
+            pyautogui.hotkey('command', 'a')
+            pyautogui.typewrite(['backspace'], 0)
+            pyautogui.typewrite("DiverseJo#00" + str(i) + "\n", 0)
+            time.sleep(2)
+    elif (len(str(i)) == 3):
+        if ((i-1)%3 != 0):
+            pyautogui.click(500, 300, 3, 0.2, button='left')
+            pyautogui.hotkey('command', 'a')
+            pyautogui.typewrite(['backspace'], 0)
+            pyautogui.typewrite("DiverseJo#0" + str(i) + "\n", 0)
+            time.sleep(2)
+        else:
+            tabrefresh(i)
+            
+            pyautogui.click(500, 300, 3, 0.2, button='left')
+            pyautogui.hotkey('command', 'a')
+            pyautogui.typewrite(['backspace'], 0)
+            pyautogui.typewrite("DiverseJo#0" + str(i) + "\n", 0)
+            time.sleep(2)
+    elif (len(str(i)) == 4):
+        if ((i-1)%3 != 0):
+            pyautogui.click(500, 300, 3, 0.2, button='left')
+            pyautogui.hotkey('command', 'a')
+            pyautogui.typewrite(['backspace'], 0)
+            pyautogui.typewrite("DiverseJo#" + str(i) + "\n", 0)
+            time.sleep(2)
+        else:
+            tabrefresh(i)
+
+            pyautogui.click(500, 300, 3, 0.2, button='left')
+            pyautogui.hotkey('command', 'a')
+            pyautogui.typewrite(['backspace'], 0)
+            pyautogui.typewrite("DiverseJo#" + str(i) + "\n", 0)
+            time.sleep(2)
---
+++ testingfile.py
@@ -0,0 +1,35 @@
+import pyautogui
+import time
+
+def tabrefresh(index):   
+    pyautogui.hotkey('command', 't') # open new tab when discord stops this from working
+    pyautogui.hotkey('command', '1') # how to implement in below?
+    pyautogui.hotkey('command', 'w')
+
+    time.sleep(1)
+
+    pyautogui.typewrite("https://discord.com/channels/@me \n", 0)
+    time.sleep(10)
+    pyautogui.click(778, 160, 1, 0, button='left') # clicks Add Friend
+    time.sleep(1)
+
+time.sleep(3)
+
+for i in range (1,7):
+    if ((i-1)%3 != 0):
+        pyautogui.click(500, 300, 3, 0.2, button='left') # click box
+        pyautogui.hotkey('command', 'a')
+        pyautogui.typewrite(['backspace'], 0) # resets if there's old names
+
+        pyautogui.typewrite("Someone_#0" + str(759) + "\n", 0)
+        time.sleep(2)
+        print("Completed task number " + str(i))
+    else:
+        tabrefresh(i)
+        
+        pyautogui.click(500, 300, 3, 0.2, button='left')
+        pyautogui.hotkey('command', 'a')
+        pyautogui.typewrite(['backspace'], 0)
+        pyautogui.typewrite("Someone_#0" + str(759) + "\n", 0)
+        time.sleep(2)
+        print("Completed task number " + str(i))
