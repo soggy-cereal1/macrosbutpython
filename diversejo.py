ed7ba57edbe9b0ff1c08eb1b054ab75a929af8ba
soggy-cereal1 <114190885+soggy-cereal1@users.noreply.github.com>
2023-01-03 20:17:09 -0500

    updated to current range

--- diversejo.py
+++ diversejo.py
@@ -21,7 +21,7 @@ pyautogui.click(778, 160, 1, 0, button='left') # clicks Add Friend
 
 # the above is extraneous if you have no friends
 
-for i in range (2656,6767):
+for i in range (2766,6767):
     if (len(str(i)) == 1):    
        if ((i-1)%3 != 0):
             pyautogui.click(500, 300, 3, 0.2, button='left')
